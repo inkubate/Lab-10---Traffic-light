c3a7f25a48acd41a69633465b2340fdb9527cbc8
inkubate <inkubate@yahoo.com>
2017-05-31 12:52:39 +0100

    Update TableTrafficLight.c

--- TableTrafficLight.c
+++ TableTrafficLight.c
@@ -2,7 +2,7 @@
 // TableTrafficLight.c for Lab 10
 // Runs on LM4F120/TM4C123
 // Index implementation of a Moore finite state machine to operate a traffic light.  
-// Daniel Valvano, Jonathan Valvano
+// Daniel Valvano, Jonathan Valvano, Chris Ringwood
 // January 15, 2016
 
 // east/west red light connected to PB5
@@ -28,11 +28,11 @@
 
 struct state {           // this is the structure
   uint16_t carlgts;      // 16 bits for the output of main lights
-	uint8_t pedlgts;      // 8 bits for the output of ped lights
+  uint8_t pedlgts;      // 8 bits for the output of ped lights
   uint16_t wait;         // wait time in the state
   uint8_t next[8];      // array for the eight possible next states
 } ;                                                     
-typedef const struct state sType;                // use the structure to create a new type called ‘sType’
+typedef const struct state sType;                // use the structure to create a new type called â€˜sTypeâ€™
 
 #define GoWs 0           // give a number to the state go west. These are called in next struct and match the sensor inputs
 #define WtWs 1           // give a number to the state wait west
@@ -65,27 +65,27 @@ int main(void){
   unsigned char cState = GoWs;          // initialise the current State to GoWs
   uint8_t input;                   // space to hold input state reading
 	
-	  sType FSM[8] = {        // create 8 instances of the new sType called ‘FSM' and pre define values
+	  sType FSM[8] = {        // create 8 instances of the new sType called â€˜FSM' and pre define values
     {(WstGRN | SthRED), 0x02, 2000, {GoWs,	GoWs,	WtWs,	WtWs,	WtWs,	WtWs,	WtWs,	WtWs} }, 					//GoWS          
     {(WstYEL | SthRED), 0x02, 1000, {GoSt,	GoSt,	GoSt,	GoSt,	BthRd,	BthRd,	BthRd,	GoSt} },   //WtWs
 		{(WstRED | SthGRN), 0x02, 2000, {GoSt,	WtSt,	GoSt,	WtSt,	WtSt,	WtSt,	WtSt,	WtSt} },  			  //GoSt
 		{(WstRED | SthYEL), 0x02, 1000, {GoWs,	GoWs,	GoWs,	GoWs,	BthRd,	BthRd,	BthRd,	BthRd} },   //WtSt
 		{(WstRED | SthRED), 0x08, 2000, {PedFl,	PedFl,	PedFl,	PedFl,	PedFl,	PedFl,	PedFl,	PedFl} },  //BthRd
-		{(WstRED | SthRED), 0x02, 1000, {PedFla,	PedFla,	PedFla,	PedFla,	PedFla,	PedFla,	PedFla,	PedFla} }, //PedFl
-		{(WstRED | SthRED), 0x00, 1000, {PedFlb,	PedFlb,	PedFlb,	PedFlb,	PedFlb,	PedFlb,	PedFlb,	PedFlb} }, //PedFla
+		{(WstRED | SthRED), 0x02, 1000, {PedFla, PedFla, PedFla, PedFla, PedFla, PedFla, PedFla, PedFla} }, //PedFl
+		{(WstRED | SthRED), 0x00, 1000, {PedFlb, PedFlb, PedFlb, PedFlb, PedFlb, PedFlb, PedFlb, PedFlb} }, //PedFla
 		{(WstRED | SthRED), 0x02, 1000, {GoWs,	GoWs,	GoSt,	GoWs,	GoWs,	GoWs,	GoSt,	GoWs} } 								//PedFlb
 
   }  ;    
 
-	portEF_init();                   // initialise ports NEEDS WRITING
+ portEF_init();                   // initialise ports NEEDS WRITING
 	
-	TExaS_Init(SW_PIN_PE210, LED_PIN_PB543210,ScopeOff); // activate grader and set system clock to 80 MHz
+  TExaS_Init(SW_PIN_PE210, LED_PIN_PB543210,ScopeOff); // activate grader and set system clock to 80 MHz
   EnableInterrupts();
 	
   while(1){
          
-		LIGHTS = FSM[cState].carlgts;      // state is from 0 to 8. Set output to val in current state
-		PEDLGTS = FSM[cState].pedlgts;      // state is from 0 to 8. Set output to val in current state
+    LIGHTS = FSM[cState].carlgts;      // state is from 0 to 8. Set output to val in current state
+    PEDLGTS = FSM[cState].pedlgts;      // state is from 0 to 8. Set output to val in current state
     delay(FSM[cState].wait);                     // wait the delay period define in current state
     input = IPREAD;                                  // check PE0 to PE2 
     cState = FSM[cState].next[input];        // cState changed to the next defined in the struct.
